7b9ae97faf95e1548a0033c6f7c1bfc8589dc368
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-07-27 21:22:44 -0400

    Adds namespacing.

--- js/practice1.js
+++ js/practice1.js
@@ -1,74 +1,84 @@
-var template = function(id) {
-	return _.template( $('#' + id).html() );
-};
+(function () {
 
-var Person = Backbone.Model.extend({
-  defaults: {
-    name: "Guest User",
-    age: 21,
-    occupation: "Student"
-  },
+  window.App = {
+    Models: {},
+    Collections: {},
+    Views: {}
+  };
+
+  window.template = function(id) {
+  	return _.template( $('#' + id).html() );
+  };
+
+  App.Models.Person = Backbone.Model.extend({
+    defaults: {
+      name: "Guest User",
+      age: 21,
+      occupation: "Student"
+    },
   
-  validate: function(attributes) {
-    if (attributes.age < 0) {
-      return 'Age must be positive.';
-    }
+    validate: function(attributes) {
+      if (attributes.age < 0) {
+        return 'Age must be positive.';
+      }
     
-    if (!attributes.name) {
-      return 'Every person must have a name.';
-    }
-  },
-  
-  work: function() {
-    return this.get('name') + 'is working.';
-  }
-});
-
-var PeopleCollection = Backbone.Collection.extend({
-  model: Person
-});
-
-var PeopleView = Backbone.View.extend({
-  tagName: 'ul',
-  
-  render: function(){
-    this.collection.each(function(person) {
-      var personView = new PersonView({model: person});
-      this.$el.append(personView.render().el);
-    }, this);
-    return this;
-  }
-});
-
-var PersonView = Backbone.View.extend({
-  tagName: 'li',
-  
-  template: template('personTemplate'),
-  
-  
-  render: function(){
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-});
-
-var peopleCollection = new PeopleCollection([
-    {
-        name: 'Gaurav Jhaveri',
-        age: 20,
-        occupation: "Programmer"
+      if (!attributes.name) {
+        return 'Every person must have a name.';
+      }
     },
-    {
-        name: 'Bruce Wayne',
-        age: 74,
-        occupation: 'Vigilante'
-    },
-    {
-        name: 'Superman',
-        age: 75,
-        occupation: 'Superhero'
+  
+    work: function() {
+      return this.get('name') + 'is working.';
     }
-]);
+  });
 
-var peopleView = new PeopleView({collection: peopleCollection});
-$(document.body).append(peopleView.render().el);
+  App.Collections.People = Backbone.Collection.extend({
+    model: App.Models.Person
+  });
+
+  App.Views.People = Backbone.View.extend({
+    tagName: 'ul',
+  
+    render: function(){
+      this.collection.each(function(person) {
+        var personView = new App.Views.Person({model: person});
+        this.$el.append(personView.render().el);
+      }, this);
+      return this;
+    }
+  });
+
+  App.Views.Person = Backbone.View.extend({
+    tagName: 'li',
+  
+    template: template('personTemplate'),
+  
+  
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  var peopleCollection = new App.Collections.People([
+      {
+          name: 'Gaurav Jhaveri',
+          age: 20,
+          occupation: "Programmer"
+      },
+      {
+          name: 'Bruce Wayne',
+          age: 74,
+          occupation: 'Vigilante'
+      },
+      {
+          name: 'Superman',
+          age: 75,
+          occupation: 'Superhero'
+      }
+  ]);
+
+  var peopleView = new App.Views.People({collection: peopleCollection});
+  $(document.body).append(peopleView.render().el);
+
+})();
